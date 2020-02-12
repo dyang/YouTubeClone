e83cb1294bb298921ae4a1993095e8d5278cafb6
Derek Yang <yanghada@gmail.com>
2020-02-12 21:43:21 +0100

    Video component - basic styling

--- components/VideoItem.js
+++ components/VideoItem.js
@@ -1,14 +1,46 @@
 import React from "react";
 import { View, Text, Image, StyleSheet } from "react-native";
+import { MaterialCommunityIcons } from "react-native-vector-icons";
+import Colors from "../constants/Colors";
+
+const subtitle = video => {
+  return (
+    video.author + " · " + video.numberOfViews + " views · " + video.createdAt
+  );
+};
 
 const VideoItem = props => {
   return (
     <View style={styles.videoContainer}>
-      <Image
-        source={{ uri: props.video.thumbnailUrl }}
-        style={styles.thumbnail}
-      />
-      <Text>{props.video.title}</Text>
+      <View style={styles.thumbnailContainer}>
+        <Image
+          source={{ uri: props.video.thumbnailUrl }}
+          style={styles.thumbnail}
+        />
+      </View>
+      <View style={styles.detailsContainer}>
+        <View style={styles.leftContainer}>
+          <Image
+            source={{ uri: props.video.authorImageUrl }}
+            style={styles.authorImage}
+          />
+        </View>
+        <View style={styles.middleContainer}>
+          <Text style={styles.title} numberOfLines={2}>
+            {props.video.title}
+          </Text>
+          <Text style={styles.subtitle} numberOfLines={2}>
+            {subtitle(props.video)}
+          </Text>
+        </View>
+        <View style={styles.rightContainer}>
+          <MaterialCommunityIcons
+            name="dots-vertical"
+            size={23}
+            color={Colors.primary}
+          />
+        </View>
+      </View>
     </View>
   );
 };
@@ -18,13 +50,46 @@ const styles = StyleSheet.create({
     flexDirection: "column",
     justifyContent: "flex-start",
     alignItems: "flex-start",
+    flex: 1
+  },
+  thumbnailContainer: {
     width: "100%",
-    height: 280,
-    backgroundColor: "#ccc"
+    height: 240
   },
   thumbnail: {
     width: "100%",
-    height: 240
+    height: "100%"
+  },
+  detailsContainer: {
+    flexDirection: "row",
+    width: "100%",
+    padding: 10
+  },
+  leftContainer: {
+    width: 40,
+    height: 40,
+    borderRadius: 20,
+    overflow: "hidden"
+  },
+  authorImage: {
+    width: "100%",
+    height: "100%",
+    resizeMode: "contain"
+  },
+  middleContainer: {
+    flex: 1,
+    flexDirection: "column",
+    paddingHorizontal: 10
+  },
+  title: {
+    fontSize: 20
+  },
+  subtitle: {
+    fontSize: 14,
+    paddingTop: 5
+  },
+  rightContainer: {
+    width: 20
   }
 });
 
