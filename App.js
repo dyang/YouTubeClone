f31822ffe0f4b39b36f9b1f84a13db52fc70380f
Derek Yang <yanghada@gmail.com>
2020-02-13 12:34:35 +0100

    Added enableScreens for better navigation performance

--- App.js
+++ App.js
@@ -1,5 +1,8 @@
 import React from "react";
+import { enableScreens } from "react-native-screens";
 import AppContainer from "./navigation/AppNavigator";
+
+enableScreens();
 
 export default function App() {
   return <AppContainer />;
